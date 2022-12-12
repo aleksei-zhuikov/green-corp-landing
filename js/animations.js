35cee554d5266c22cbed734a448d313146949654
Aleksei Zhuikov <design.azv@gmail.com>
2022-12-12 19:07:30 +0300

    added select other price

--- js/animations.js
+++ js/animations.js
@@ -24,3 +24,40 @@ function initIncreaseNumberAnimation() {
 
 }
 initIncreaseNumberAnimation();
+
+
+// Форма выбора цены раздел другое
+
+const SELECT_BUDGET = document.querySelector('#budget');
+const INPUT = document.createElement('input');
+const FORM = document.querySelector('#form');
+const FORM_BUTTON = document.querySelector('.form__submit');
+
+
+SELECT_BUDGET.addEventListener('change', function handleSelectChange(event) {
+
+
+
+  if (event.target.value === 'other') {
+    let formContainer = document.createElement('div');
+    formContainer.classList.add('form__group', 'form__other-input');
+
+
+    INPUT.placeholder = 'Введите ваш вариант';
+    INPUT.type = 'text';
+
+    formContainer.appendChild(INPUT);
+
+    FORM.insertBefore(formContainer, FORM_BUTTON);
+  };
+
+  // удаляем ранее добавленное полеб если оно есть в DOM
+
+  const OTHER_INPUT = document.querySelector('.form__other-input');
+
+  if (event.target.value !== 'other' && Boolean(OTHER_INPUT)) {
+
+    FORM.removeChild(OTHER_INPUT)
+
+  }
+});
